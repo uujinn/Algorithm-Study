d0e6ce30883a1d80f9ef7a3a745cdd6f8f4a4337
n-y-kim <nyn2265@gmail.com>
2022-01-15 17:51:42 +0900

    Pass: 신규 아이디 추천

---
+++ Programmers/신규 아이디 추천-NY.py
@@ -0,0 +1,43 @@
+def solution(new_id):
+    #step 1
+    step_1_answer = list(new_id.lower())
+    
+    #step 2 & 3
+    special_char = {"-":1, "_":1, ".":1}
+    answer = []
+    behind_letter = ""
+    
+    while len(step_1_answer) != 0:
+        popped = step_1_answer.pop()
+        
+        alphabet_check = ord(popped) >= 97 and ord(popped) <= 122
+        number_check = ord(popped) >= 48 and ord(popped) <= 57
+        
+        if popped in special_char or alphabet_check or number_check:
+            if popped == '.' and behind_letter == '.': continue
+            else: answer.append(popped)
+        
+        behind_letter = answer[-1] if len(answer) != 0 else ""
+        
+    answer.reverse()
+    
+    #step 4
+    if len(answer) > 1:    
+        if answer[0] == '.': answer.pop(0)
+        if answer[-1] == '.': answer.pop()
+    elif len(answer) == 1:
+        if answer[0] == '.': answer.pop()
+        
+    
+    #step 5
+    if len(answer) == 0: answer.append("a") 
+    
+    #step 6
+    answer = answer[0:15] if len(answer) >= 16 else answer
+    if answer[-1] == '.': answer.pop()
+    
+    #step 7
+    if len(answer) < 3: 
+        answer.append(answer[-1] * (3 - len(answer)))
+    
+    return ''.join(answer)
