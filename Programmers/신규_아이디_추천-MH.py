849e517f33b36c2b3b41bcc906b8c311f2b8b7d3
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-01-16 23:23:48 +0900

    Update: 신규 아이디 추천 MH
    간결하게 코드 정리

--- Programmers/신규_아이디_추천-MH.py
+++ Programmers/신규_아이디_추천-MH.py
@@ -1,61 +1,36 @@
-def commaTrim(str):
-    trim_str = list(str)
-    
-    if (len(trim_str) <= 1):
-        if (trim_str == '.'):
-            return ''
-        else:
-            return trim_str
-        
-    while (len(trim_str) > 1):
-        if (trim_str[0] == '.'):
-            trim_str[0] = ''
-        if (trim_str[-1] == '.'):
-            trim_str[-1] = ''
-            
-        if not (trim_str[0] == '.' and trim_str[len(trim_str) - 1] == '.'):
-            break
-        
-        trim_str = [i for i in trim_str if i]
-   
-    return trim_str
-
 def solution(new_id):
     # 1단계
-    answer = list(new_id.lower())
+    new_id = new_id.lower()
     
     # 2단계
-    for i in range(len(answer)):
-        char = answer[i]
-        if not (char.isalnum() or char == '-' or char == '_' or char == '.'):
-            answer[i] = ""
-    
-    answer = [i for i in answer if i]   # 리스트 내 공백 제거
+    answer = ''
+    for word in new_id:
+        if word.isalnum() or word in '-_.':
+            answer += word
     
     # 3단계
-    split_str = "".join(answer)          # list to str
-    split_str = split_str.split('.')
-    split_str = [i for i in split_str if i]
-    answer = '.'.join(split_str)
+    while '..' in answer:
+        answer = answer.replace('..', '.')
     
     # 4단계
-    answer = commaTrim(answer)
+    if answer[0] == '.' and len(answer) > 1:
+        answer = answer[1:]
+    if answer[-1] == '.':
+        answer = answer[:-1]
         
     # 5단계
-    if (len(answer) == 0):
+    if answer == '':
         answer = 'a'
     
     # 6단계
-    if (len(answer) >= 16):
+    if len(answer) >= 16:
         answer = answer[:15]
-    answer = commaTrim(answer)
+    if answer[-1] == '.':
+        answer = answer[:-1]
     
     # 7단계
-    while (len(answer) < 3):
-        char = answer[-1]
-        answer.append(char)
-    
-    answer = "".join(answer)          # list to str
+    while len(answer) < 3:
+        answer += answer[-1]
     
     return answer
 
