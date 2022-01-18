3ae00cd5ad86e57e1e1a3fcca15f3e33675266f4
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-01-18 21:17:16 +0900

    Update: Programmers 숫자 문자열과 영단어

--- Programmers/숫자_문자열과_영단어-YJ.py
+++ Programmers/숫자_문자열과_영단어-YJ.py
@@ -1,7 +1,7 @@
 def solution(s):
-    num_dict = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
-    
-    for key, value in num_dict.items():
-        s = s.replace(key, str(value))   
+    num_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for num in num_list:
+        s = s.replace(num, str(num_list.index(num)))
 
     return int(s)
