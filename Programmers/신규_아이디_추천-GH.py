530645a68b6e38fa9cc05c4430878d07ac58d0c8
hyeon <2019112529@mme.dongguk.edu>
2022-01-18 23:14:06 +0900

    Pass: 숫자 문자열과 영단어-GH

--- Programmers/신규_아이디_추천-GH.py
+++ Programmers/신규_아이디_추천-GH.py
@@ -4,7 +4,16 @@ def solution(new_id):
     answer = ''
     
     # 1. Upper Case -> Lower Case
-    # 2. 소문자, 숫자, -, _, . 제외 모두 제거 
+    # 2. 소문자, 숫자, -, _, . 제외 모두 제거
+    """
+    for num in set(answer):
+        if (ord(num) > 96 and ord(num) < 123) or\
+            (ord(num) > 47 and ord(num) < 58) or\
+                ord(num) == 95 or ord(num) == 45 or ord(num) == 46:
+                    pass
+        else:
+            answer = answer.replace(answer[answer.index(num), ""])
+    """
     
     answer = re.sub('[^a-z\d\-\_\.]', '', new_id.lower())
     """
@@ -14,23 +23,42 @@ def solution(new_id):
     """
     
     # 3. 마침표가 2번 이상이면 하나로 치환
-    #마침표가 2번 이상 (\.\.+)
+    while('..' in answer):
+        answer = answer.replace("..", ".")
+    """
+    마침표가 2번 이상 (\.\.+)
     answer = re.sub('\.\.+', ',', answer)
-    
+    """
     
     # 4. 마침표가 처음이나 끝이라면 제거
-    # 마침표가 처음(^\.)이나 끝(\.$)
+    answer = answer.strip('.')  
+    # .strip([chars]): 인자로 전달된 문자를 String의 좌우에서 제거
+    """
+    마침표가 처음(^\.)이나 끝(\.$)
     answer = re.sub('^\.\.$', '', answer)
+    """
     
     # 5. 빈 문자열이라면, a를 대입
     if answer == '':
         answer = 'a'
     
     # 6. 16자 이상이면 뒤에서부터 제거, 제거 후 마침표가 마지막 문자면 마침표 제거
+    if len(answer) > 15:
+        answer = answer[:15]
+        answer = answer.rstrip('.')
+    """
     answer = re.sub('\.$', '', answer[0:15])
-          
+    """
+        
     # 7. 2자 이하면 마지막 문자 반복
     while len(answer) < 3:
         answer += answer[-1:]
         
     return answer
+
+"""
+정규표현식 공부하다가 기 빨려서 포기...
+근데 특히 step 2에서 정규표현식 말고 어떻게 했나 궁금해서
+다른 답안 참고해서 아스키 코드로 구현한 코드는 또 런타임 에러 남...
+뭔가 잘못된 거겠지...
+"""
