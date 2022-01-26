20ee41de69eb352f567752f12ae7a176393e3e47
n-y-kim <nyn2265@gmail.com>
2022-01-26 11:42:53 +0900

    Update: BOJ 1874 스택 수열

--- BOJ/Data_Structure/1874-스택_수열-NY.py
+++ BOJ/Data_Structure/1874-스택_수열-NY.py
@@ -2,7 +2,7 @@ from sys import stdin
 n = int(stdin.readline())
 stack = [] #스택 저장
 seq = [] #만들어야 하는 수열 저장
-answer = [] #+,- 저장
+answer = "" #+,- 저장
 cnt = 0 #오름차순으로 숫자 더해서 stack에 넣어줄 숫자
 for i in range(n):
     seq.append(int(stdin.readline()))
@@ -14,12 +14,11 @@ for s in seq:
             exit()
         elif not len(stack) or stack[-1] < s: #스택 길이가 0이거나 스택 마지막 값이 수열값보다 작을때
             cnt += 1
-            answer.append("+")
             stack.append(cnt)
+            answer += "+\n"
     
     #s == stack[-1]. while문이 끝나면 무조건 둘의 값은 같음
-    answer.append("-")
+    answer += "-\n"
     stack.pop()
     
-for a in answer:
-    print(a)
+print(answer, end='')
