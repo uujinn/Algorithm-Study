8cf6625ced19c01cdcacf3a3372cf3907c09bad2
n-y-kim <nyn2265@gmail.com>
2022-02-07 22:47:16 +0900

    Update: BOJ 2504 괄호의 값

--- BOJ/Data_Structure/2504-괄호의_값-NY.py
+++ BOJ/Data_Structure/2504-괄호의_값-NY.py
@@ -1,8 +1,10 @@
-import enum
 from sys import stdin
 brackets = str(stdin.readline().rstrip())
+
 stack = [brackets[0]] #첫 괄호를 무조건 넣고 시작
+
 if brackets[0] in ')]': brackets = "" #만약 첫 괄호가 닫는 괄호라면 무조건 틀린 괄호임. brackets을 지워서 for문을 돌지 않도록 조정
+
 number = {'[':'3', '(':'2', ']':'3', ')':'2'}
 popped, pop_cnt, app_cnt = False, 0, 1 #바로 직전에 popped 유무에 따라 더해주는 문자가 달라짐. pop_cnt와 app_cnt는 pop과 append 한 횟수가 같아야 괄호들이 쌍을 잘 이룬것이기 때문에 추가함.
 exp = "" #마지막에 eval에 사용될 식
