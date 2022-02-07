501b3d762b0181a22baaa3b987e59cf26d9d8b1b
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-02-08 03:22:06 +0900

    Update: Programmers K번째수

--- Programmers/K번째수-MH.py
+++ Programmers/K번째수-MH.py
@@ -1,14 +1,9 @@
 def solution(array, commands):
     answer = []
     
-    for com in commands:
-        i, j, k = com[0] - 1, com[1] - 1, com[2] - 1
-        
-        temp = array[i:j + 1]
-        temp.sort()
-        index = k % len(temp)        
-        answer.append(temp[index])
-    
+    for i, j, k in commands:
+        temp = sorted(array[i - 1:j])
+        answer.append(temp[k - 1])    
     return answer
 
 print(solution([1, 5, 2, 6, 3, 7, 4],	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
