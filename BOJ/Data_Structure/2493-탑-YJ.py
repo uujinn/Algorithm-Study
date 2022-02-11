ccf739801c92a286750e24d84888eab4c1b36a79
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-02-11 22:12:19 +0900

    Update: Boj 2493 탑
    Print문 삭제

--- BOJ/Data_Structure/2493-탑-YJ.py
+++ BOJ/Data_Structure/2493-탑-YJ.py
@@ -8,9 +8,6 @@ answer = [0] * N # default 값 = 0
 for i, tower in enumerate(reversed(towers)):
     index = len(towers) - i - 1   # 탑의 인덱스 4, 3, 2, 1, 0
     while indexs: # 현재 쏘고 있는 신호 있음
-        # print(f'{index}: {tower}')
-        # print(indexs)
-        # print(towers[indexs[-1]])
         if tower > towers[indexs[-1]]: # 해당 신호를 보낸 탑의 높이보다 수신할 차례인 탑이 키가 크다면 
             answer[indexs.pop()] = index + 1 # 신호 자리에 해당 신호를 수신한 탑의 번호 저장 5, 4, 3, 2, 1
         else: # 수신 불가 
