81e4c474ed719ae72d5dcb3a419e807fb73d21b4
n-y-kim <nyn2265@gmail.com>
2022-02-14 19:55:13 +0900

    Pass: BOJ 22942 데이터 체커

--- BOJ/Data_Structure/22942-데이터_체커-NY.py
+++ BOJ/Data_Structure/22942-데이터_체커-NY.py
@@ -3,17 +3,17 @@ N = int(stdin.readline())
 circles = []
 stack = []
 for _ in range(N):
-    circles.append(list(map(int, stdin.readline().split())))
+    x, r = map(int, stdin.readline().split())
+    circles.append([x-r, x+r]) #각 원의 왼쪽 끝점과 오른쪽 끝점을 저장
     
-for circle in sorted(circles, key=lambda circle: circle[1] ,reverse=True):
-    if not stack: stack.append(circle)
+for circle in sorted(circles): #sorted를 통해 왼쪽 점들 기준으로 정렬. 가장 왼쪽에 가까운 점들부터 처리.
+    if not stack: stack.append(circle) #만약 스택이 비어있으면(처음이면) 추가
     else:
-        before = stack.pop()
-        x_diff = abs(circle[0] - before[0]) 
-        if circle[1] + before[1] < x_diff or abs(circle[1] - before[1]) > x_diff or x_diff == 0:
+        #circle이 스택 가장 위에 있는 놈이랑 비교했을때 위에 놈 [왼, 오] 사이에 들어가거나, 위에 놈 오른쪽보다 circle의 왼,오가 다 커야됨.
+        if (circle[0] > stack[-1][0] and circle[1] < stack[-1][1]) or (circle[0] > stack[-1][1] and circle[1] > stack[-1][1]): 
             stack.append(circle)
         else:
             print("NO")
             exit()
-
+            
 print("YES")
