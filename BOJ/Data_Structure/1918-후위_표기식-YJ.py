bef894d64da3b52a2887eb5606135805331fe8ac
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-02-17 00:08:26 +0900

    Pass: Boj 1918 후위 표기식

---
+++ BOJ/Data_Structure/1918-후위_표기식-YJ.py
@@ -0,0 +1,37 @@
+from sys import stdin
+from tabnanny import check
+
+# 연산자 우선순위
+def check_priority(s):
+    if s == '(' or s == ')': return 0
+    elif s == '+' or s == '-': return 1
+    elif s == '*' or s == '/': return 2
+    else: return -1
+
+expression = stdin.readline().rstrip()
+stack = []
+answer = ""
+
+for s in expression:
+    if s == '(': 
+        stack.append(s)
+    elif s == ')': # '(' 나올 때까지 answer에 더해줌
+        while stack:
+            tmp = stack.pop()
+            if tmp == '(':  break
+            else:   
+                answer += tmp
+    elif s in '+-*/':
+        while stack:
+            tmp = stack[-1]
+            if check_priority(s) <= check_priority(tmp): # 우선순위 확인
+                answer += stack.pop()
+            else:   break
+        stack.append(s)
+    else: # 알파벳이면 answer에 더해줌
+        answer += s
+
+while stack: # 남아있는 연산자 출력
+    answer += stack.pop()
+
+print(answer)
