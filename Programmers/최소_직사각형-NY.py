e4f53c710bc8fc8c6e113c979639d63f8b1311f3
n-y-kim <nyn2265@gmail.com>
2022-03-03 22:50:56 +0900

    Non-Pass: Programmers 최소 직사각형

---
+++ Programmers/최소_직사각형-NY.py
@@ -0,0 +1,10 @@
+def solution(sizes):
+    max_sqr = sizes[0]
+    for size in sizes: #그리디로 구상. 맥스 값이랑 비교하면서 계속 최대값을 고쳐나가고 마지막 값을 리턴
+        if size[0] > max_sqr[0] and ~ : #단순 가로/세로 길이 비교 외에도 가로-세로 세로-가로 길이 비교도 필요하다고 생각해서 조건 and 넣었습니다
+            
+        if size[1] > max_sqr[1] and ~ :
+            
+    return answer
+
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
