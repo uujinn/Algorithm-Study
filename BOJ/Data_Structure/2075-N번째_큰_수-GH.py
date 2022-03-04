7dee895f9b0e33cd8c0722cbd7d1821351def9c7
hyeon <2019112529@mme.dongguk.edu>
2022-03-05 00:35:54 +0900

    Pass: Boj 2075 N번째 큰 수

---
+++ BOJ/Data_Structure/2075-N번째_큰_수-GH.py
@@ -0,0 +1,22 @@
+import sys
+import heapq
+
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    nums = list(map(int, input().split()))
+
+    if not heap:
+        for num in nums:
+            heapq.heappush(heap, num)
+
+    else:
+        for num in nums:
+            if heap[0] < num:
+                heapq.heappush(heap, num)
+                heapq.heappop(heap)
+
+print(heap[0])
