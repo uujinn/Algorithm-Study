a26f28ac918bd9d02254619564481a721367e938
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-03-04 20:37:56 +0900

    Revert "Pass: Boj 2075 N번째 큰 수"
    This reverts commit f90ad0357cf1f5cdfee649d5464186d06059fa1f.

--- Programmers/2075-N번째_큰_수.py
+++ Programmers/2075-N번째_큰_수.py
@@ -1,19 +1,14 @@
 from sys import stdin
 from heapq import heappop, heappush
 
-heap = []                   # 길이 N을 유지하는 최소 힙
+heap = []       # 최대 힙
 N = int(stdin.readline())
 
 for _ in range(N):
-    nums = list(map(int, stdin.readline().split()))
-    
-    if not heap:            # heap에 값이 없으면 입력받은 5개 값 모두 넣어줌
-        for num in nums:
-            heappush(heap, num)
-    else:
-        for num in nums:
-            if heap[0] < num:
-                heappop(heap)           # heap의 최소값 제거
-                heappush(heap, num)
+    for num in map(int, stdin.readline().split()):
+        heappush(heap, num * (-1))
 
-print(heap[0])              # 길이 N인 힙에서 최소값 추출 (N번째로 큰 수)
+for _ in range(N - 1):
+    heappop(heap)
+
+print(heappop(heap) * (-1))
