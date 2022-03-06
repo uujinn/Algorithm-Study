2c345f12ee794c98aff8a663508b5db9bc9fb72f
n-y-kim <nyn2265@gmail.com>
2022-03-07 08:38:25 +0900

    Pass: BOJ 7662 이중 우선순위 큐

--- BOJ/Data_Structure/7662-이중_우선순위_큐-NY.py
+++ BOJ/Data_Structure/7662-이중_우선순위_큐-NY.py
@@ -14,18 +14,18 @@ for _ in range(T):
             heapq.heappush(heap_min, (num, i))
         elif letter=="D" and heap_max and heap_min: #비어 있지 않을때
             if num == -1: #최소값 제거
-                numbers[heap_min[0][1]] = True
-                heapq.heappop(heap_min)
+                while heap_min and numbers[heap_min[0][1]]: heapq.heappop(heap_min)
+                if heap_min:
+                    numbers[heap_min[0][1]] = True
+                    heapq.heappop(heap_min)
             else: #최대값 제거
-                numbers[heap_max[0][1]] = True
-                heapq.heappop(heap_max)
-            
-            for j in heap_min:
-                if numbers[j[1]]: heapq.heappop(heap_min)
-                else: heapq.heappushpop(heap_min, j)
-            for j in heap_max:
-                if numbers[j[1]]: heapq.heappop(heap_max)
-                else: heapq.heappushpop(heap_max, j)
+                while heap_max and numbers[heap_max[0][1]]: heapq.heappop(heap_max)
+                if heap_max:
+                    numbers[heap_max[0][1]] = True
+                    heapq.heappop(heap_max)
+                    
+    while heap_min and numbers[heap_min[0][1]]: heapq.heappop(heap_min)
+    while heap_max and numbers[heap_max[0][1]]: heapq.heappop(heap_max) #만약 D -1 만 계속 나왔다면 동기화가 안되어 있을 수 있어서 마지막에 한 번 더 해줌
             
     if heap_max and heap_min: print("%d %d" %(-heap_max[0][0], heap_min[0][0]))
     else: print("EMPTY")
