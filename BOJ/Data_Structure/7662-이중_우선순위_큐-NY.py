49db84359d5de5b16f3671b0f6db944a8aacbf85
n-y-kim <nyn2265@gmail.com>
2022-03-06 23:02:55 +0900

    Non-Pass: BOJ 7662 이중 우선순위 큐

--- BOJ/Data_Structure/7662-이중_우선순위_큐-NY.py
+++ BOJ/Data_Structure/7662-이중_우선순위_큐-NY.py
@@ -2,18 +2,30 @@ from sys import stdin
 import heapq
 T = int(stdin.readline())
 for _ in range(T):
-    heap = []
+    heap_max = []
+    heap_min = []
     k = int(stdin.readline())
-    for _ in range(k):
+    numbers = [False] * k
+    for i in range(k):
         letter, num = map(str, stdin.readline().split())
+        num = int(num)
         if letter=="I":
-            heapq.heappush(heap, int(num))
-        elif letter=="D" and heap: #비어 있지 않을때
-            if int(num) == -1: #최소값 제거
-                heapq.heapify(heap)
-                heapq.heappop(heap) 
+            heapq.heappush(heap_max, (-num, i))
+            heapq.heappush(heap_min, (num, i))
+        elif letter=="D" and heap_max and heap_min: #비어 있지 않을때
+            if num == -1: #최소값 제거
+                numbers[heap_min[0][1]] = True
+                heapq.heappop(heap_min)
             else: #최대값 제거
-                heapq._heapify_max(heap)
-                heapq.heappop(heap)
-    if heap: print("%d %d" %(max(heap), min(heap)))
+                numbers[heap_max[0][1]] = True
+                heapq.heappop(heap_max)
+            
+            for j in heap_min:
+                if numbers[j[1]]: heapq.heappop(heap_min)
+                else: heapq.heappushpop(heap_min, j)
+            for j in heap_max:
+                if numbers[j[1]]: heapq.heappop(heap_max)
+                else: heapq.heappushpop(heap_max, j)
+            
+    if heap_max and heap_min: print("%d %d" %(-heap_max[0][0], heap_min[0][0]))
     else: print("EMPTY")
