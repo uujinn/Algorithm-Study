caf011aa59eaf495835a92a28e53cee0484def13
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-03-08 00:33:06 +0900

    Non-Pass: Boj 7662 이중 우선순위 큐
    난 정말 실어........ 백준과 나와 궁합 24% 딱 거기까지....... 반례도 다 맞는데 어디서 틀린걸까요
    문제 추천 시스템 ver1이랑 비슷한 로직으로 풀었는데 둘다 틀린걸 보면 같은 부분이 문제인 것 같은데..

--- BOJ/Data_Structure/7662-이중_우선순위_큐-MH.py
+++ BOJ/Data_Structure/7662-이중_우선순위_큐-MH.py
@@ -1,29 +1,48 @@
 from sys import stdin
-import heapq
+from heapq import heappush, heappop
 
 answer = ''
-
 for _ in range(int(stdin.readline())):          # T번 반복
-    heap = []                                   # 오름차순으로 정렬된 deque
+    min_heap = []                               # 최솟값 출력 위한 heap
+    max_heap = []                               # 최댓값 출력 위한 heap
+    deleted = {}
     
-    for _ in range(int(stdin.readline())):      # K번 반복
+    for i in range(int(stdin.readline())):      # K번 반복
         cmd, val = stdin.readline().split()
+        val = int(val)
 
         if cmd == 'I':
-            heapq.heappush(heap, int(val))
-        elif heap and cmd == 'D':
-            if val == '-1':             # 최소값 제거
-                heapq.heappop(heap)
-            elif val == '1':            # 최대값 제거
-                heap.pop(heap.index(heapq.nlargest(1, heap)[0]))
+            heappush(min_heap, (val, i))
+            heappush(max_heap, (-val, i))
+            deleted[i] = False
+            
+        elif min_heap and max_heap and cmd == 'D':
+            if val == -1 and min_heap:             # 최소값 제거
+                min_val = heappop(min_heap)[1]
+                deleted[min_val] = True
+                
+            elif val == 1 and max_heap:            # 최대값 제거
+                max_val = -heappop(max_heap)[1]
+                deleted[max_val] = True
+                    
+        while min_heap and deleted[min_heap[0][1]]:
+            # print(heappop(min_heap))
+            heappop(min_heap)
+        while max_heap and deleted[max_heap[0][1]]:
+            # print(heappop(max_heap))
+            heappop(max_heap)
+                    
+        # print('min heap : ', end='')
+        # print(min_heap)
+        # print('max heap : ', end='')
+        # print(max_heap)
+        # print('deleted : ', end='')
+        # print(deleted)
+        # print('-------------')
         
-        print(heap)
-        
-    if not heap:
+    if not min_heap or not max_heap:
         answer += 'EMPTY\n'
     else:
-        answer += (str(heapq.nlargest(1, heap)[0]) + ' ' + str(heap[0]) + '\n')
-
-    print('-----')
-
-print(answer)
+        answer += (str(-max_heap[0][0]) + ' ' + str(min_heap[0][0]) + '\n')
+        
+print(answer.rstrip())
