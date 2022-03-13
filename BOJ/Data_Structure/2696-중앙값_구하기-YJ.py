795766b78eb1a71d39afc57d34a997902056ae52
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-03-13 15:50:19 +0900

    Pass: Boj 2696 중앙값 구하기

--- BOJ/Data_Structure/2696-중앙값_구하기-YJ.py
+++ BOJ/Data_Structure/2696-중앙값_구하기-YJ.py
@@ -1,4 +1,3 @@
-import enum
 from sys import stdin
 from heapq import heappop, heappush
 
@@ -21,10 +20,25 @@ for _ in range(T):
 
     print((len(arr) // 2 ) + 1) # 중앙값 개수
     median = arr[0] # 첫번째 중앙값은 무조건 첫번째 값 
+    medians.append(median)
 
-    for idx, value in enumerate(arr[1:]):
+    for idx, value in enumerate(arr[1:], 1):
         if value > median: # 중앙값보다 큰 값은 오른쪽
             heappush(right_h, value)
         else: # 중앙값보다 작은 값은 왼쪽
             heappush(left_h, -value)
         
+        if idx % 2 == 0:
+            if len(right_h) > len(left_h):
+                heappush(left_h, -median) # 최대힙이므로 음수
+                median = heappop(right_h) # 중앙값 갱신
+            elif len(right_h) < len(left_h): # * 그냥 else로 두면 안됨 (길이 같을 경우)
+                heappush(right_h, median)
+                median = -heappop(left_h)
+            medians.append(median)
+    
+    idx = 0
+    for _ in range(len(medians) // 10):
+        print(*medians[idx:idx+10])
+        idx += 10
+    print(*medians[idx:])
