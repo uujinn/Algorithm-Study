207ea147981a94417855b541680217d76b672ad0
n-y-kim <nyn2265@gmail.com>
2022-03-15 06:27:46 +0900

    Pass: BOJ 21942 부품 대여장

--- BOJ/Data_Structure/21942-부품_대여장-NY.py
+++ BOJ/Data_Structure/21942-부품_대여장-NY.py
@@ -1,27 +1,29 @@
 from sys import stdin
 from datetime import datetime
+from collections import defaultdict
+
 N, L, F = map(str, stdin.readline().split())
 rents = {}
-fined = {}
+fined = defaultdict(int)
 N, F = int(N), int(F)
-days = int(L.split("/")[0])
-hours, minutes = map(int, L.split("/")[1].split(":"))
-total_sec = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60
+total_sec = int(L[:3]) * 24 * 60 * 60 + int(L[4:6]) * 60 * 60 + int(L[7:]) * 60
+
 for _ in range(N):
     date, time, P, M = map(str, stdin.readline().split())
-    if (P, M) in rents:
+    if (P, M) in rents and rents[(P, M)] != 0:
         return_t = datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M")
         borrow_t = rents[(P,M)]
         
-        cnt = int(((return_t - borrow_t).total_seconds() - total_sec)/60 * F)
+        cnt = int(((return_t - borrow_t).total_seconds() - total_sec)//60 * F)
         
         if cnt > 0:
-            if M in fined: fined[M] += cnt
-            else: fined[M] = cnt
+            fined[M] += cnt
+            
+        rents[(P,M)] = 0 #처음에 생각 못 한 부분. 같은 사람이 같은 물건을 다시 빌리게 될 때를 고려해서 0으로 다시 바꿔야됨
     else:
         rents[(P, M)] = datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M")
         
 if not fined: print(-1)
 else:
-    for f in sorted(fined.items(), key=lambda x : x[1]):
+    for f in sorted(fined.items()):
         print(f[0], f[1])
