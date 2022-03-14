9d271e46f3c80c998b480b9bdb38969a529cb7dc
n-y-kim <nyn2265@gmail.com>
2022-03-14 23:20:00 +0900

    Non-Pass: BOJ 21942 부품 대여장

---
+++ BOJ/Data_Structure/21942-부품_대여장-NY.py
@@ -0,0 +1,25 @@
+from sys import stdin
+from datetime import datetime
+N, L, F = map(str, stdin.readline().split())
+rents = {}
+fined = []
+N, F = int(N), int(F)
+days = int(L.split("/")[0])
+hours, minutes = map(int, L.split("/")[1].split(":"))
+total_sec = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60
+for _ in range(N):
+    date, time, P, M = map(str, stdin.readline().split())
+    if (P, M) in rents:
+        return_t = datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M")
+        borrow_t = rents[(P,M)]
+        
+        cnt = int(((return_t - borrow_t).total_seconds() - total_sec)/60 * F)
+        
+        if cnt > 0: fined.append((M, cnt))
+    else:
+        rents[(P, M)] = datetime.strptime(date + " " + time, "%Y-%m-%d %H:%M")
+        
+if not fined: print(-1)
+else:
+    for f in sorted(fined):
+        print(f[0], f[1])
