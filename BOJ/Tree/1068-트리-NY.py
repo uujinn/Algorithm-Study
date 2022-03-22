18fa3ea3bb2adb8a2b2e4b4258549adaf857854d
n-y-kim <nyn2265@gmail.com>
2022-03-23 08:20:11 +0900

    Pass: BOJ 1068 트리

--- BOJ/Tree/1068-트리-NY.py
+++ BOJ/Tree/1068-트리-NY.py
@@ -1,44 +1,21 @@
 import sys
 sys.setrecursionlimit(int(100000))
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-def DFS(root, visited, target):
-    if root is None or visited[root.data]: return
-    else:
-        visited[root.data] = True
-        if root.data == target:
-            root = None
-            return
-        if root.left is None and root.right is None:
-            cnt[0] += 1
-            return
-    DFS(root.left, visited, target)
-    if root.left is None and root.right is None:
-        cnt[0] += 1
-    DFS(root.right, visited, target)
+def DFS(parents, target):
+    parents[target] = None
+    for idx, p in enumerate(parents):
+        if p is target:
+            DFS(parents, idx)
 
 N = int(sys.stdin.readline())
 parents = list(map(int, sys.stdin.readline().split()))
 target = int(sys.stdin.readline())
+count = 0
 
-Tree = [0] * N
-visited = [False] * N
-cnt = [0]
+DFS(parents, target)
 
-Tree[0] = Node(0)
+for i in range(N):
+    if parents[i] != None and i not in parents:
+        count += 1
 
-for idx, parent in enumerate(parents):
-    if idx == 0: continue
-    Tree[idx] = Node(idx)
-    if Tree[parent].left is None:
-        Tree[parent].left = Tree[idx]
-    else: Tree[parent].right = Tree[idx]
-
-DFS(Tree[0], visited, target)
-
-print(cnt[0])
+print(count)
