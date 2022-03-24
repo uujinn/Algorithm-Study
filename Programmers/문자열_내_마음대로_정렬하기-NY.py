54160ad749d571bf5072fb6a590463ea9439b479
n-y-kim <nyn2265@gmail.com>
2022-03-24 09:26:26 +0900

    Update: Programmers 문자열 내 마음대로 정렬하기 -> 한줄

--- Programmers/문자열_내_마음대로_정렬하기-NY.py
+++ Programmers/문자열_내_마음대로_정렬하기-NY.py
@@ -1,6 +1,2 @@
 def solution(strings, n):
-    strings_word = [[] for _ in range(len(strings))]
-    for i in range(len(strings)):
-        strings_word[i].append(strings[i])
-        strings_word[i].append(strings[i][n])
-    return list(s[0] for s in sorted(strings_word, key=lambda x: (x[1], x[0])))
+    return list(s for s in sorted(sorted(strings), key=lambda x: x[n]))
