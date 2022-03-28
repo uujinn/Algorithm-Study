5632f18d719f4d62f11b066b46481ac9ec04f673
n-y-kim <nyn2265@gmail.com>
2022-03-29 00:00:29 +0900

    Update: BOJ 6419 트리인가?

--- BOJ/Tree/6416-트리인가?-NY.py
+++ BOJ/Tree/6416-트리인가?-NY.py
@@ -1,17 +1,25 @@
 from sys import stdin
 case = 1
+lines = []
+temp = ""
 while True:
-    temp = []
+    temp += stdin.readline().rstrip()
+    if temp[-5:] == '-1 -1': 
+        lines.append(temp[:-5])
+        break
+    elif temp[-1] == " ": 
+        lines.append(temp)
+        temp = ""
+    else: temp += "  "
+
+for line in lines:
+    u_vs = list(map(str, line.split("  ")))
     tree = {}
     v_flag = {}
     result = True
-    while True:
-        temp += list(map(str, stdin.readline().rstrip().split("  ")))
-        if temp[-1] == '0 0': continue
-        elif temp[-2] == '0 0': break
     
-    for t in temp[:-1]:
-        u, v = t.split()
+    for u_v in u_vs[:-1]:
+        u, v = u_v.split()
         if v in v_flag: #이미 다른 u에서 v로 들어오는 간선이 있는데 또 새로 들어갈 경우
             result = False
             break
@@ -25,10 +33,8 @@ while True:
             if key not in v_flag: 
                 result = True
     
-    if temp[0] == '0 0': result = True
+    if u_vs[0] == '0 0': result = True
     
     if not result: print("Case %d is not a tree." % case)
     else: print("Case %d is a tree." % case)
     case += 1
-    
-    if temp[-1] != '': exit(0)
