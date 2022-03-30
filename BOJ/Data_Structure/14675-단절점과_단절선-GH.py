8950fae510cadade59a1c2c9a0e2080115b5f51f
hyeon <97775368+0ee9@users.noreply.github.com>
2022-03-30 23:57:05 +0900

    Pass: Boj 14675 단절점과 단절선

---
+++ BOJ/Data_Structure/14675-단절점과_단절선-GH.py
@@ -0,0 +1,21 @@
+import sys
+
+N = int(input())
+tree = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    a, b = map(int, sys.stdin.readline().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+q = int(input())
+for _ in range(q):
+    t, k = map(int, sys.stdin.readline().split())
+    if t == 1:
+        if (len(tree[k]) < 2):
+            print("no")
+        else:
+            print("yes")
+
+    elif t == 2:
+        print("yes")
