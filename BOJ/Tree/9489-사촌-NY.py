f102e93eae9679ea3b852f68f8367fcb3f5c86da
n-y-kim <nyn2265@gmail.com>
2022-04-10 10:16:20 +0900

    Update & Non-Pass: BOJ 9489 사촌

--- BOJ/Tree/9489-사촌-NY.py
+++ BOJ/Tree/9489-사촌-NY.py
@@ -1,27 +1,36 @@
 from sys import stdin
 cousins = []
 
-def dfs_k(nodes, k):
-    while  
-
-def find_cousins():
+def find_cousins(nodes, k):
+    k_parent, k_depth = map(int, nodes[k])
+    count = 0
+    for key, val in nodes.items():
+        if val[1] == k_depth and val[0] != k_parent:
+            count += 1
+    cousins.append(count)
     
-
 while True:
     n, k = map(int, stdin.readline().split())
     if n == k == 0: break
-    nodes = {}
     nums = list(map(int, stdin.readline().split()))
+    nodes = {nums[0]:[None, 0]}
     temp = []
-    for idx, num in enumerate(nums):
-        if num not in nodes:
-            nodes[num] = []
-        if nums[idx-1]+1 != num: #연속 끊겼음
-            for key, val in sorted(nodes.items()):
-                if len(val) == 0: 
-                    nodes[key].extend(temp)
-                    break
+    prev_prnt_idx = 0
+    for idx in range(1, n):
+        if nums[idx] not in nodes:
+            nodes[nums[idx]] = []
+        if idx == n-1 or nums[idx+1]-1 != nums[idx]: #연속 끊겼음
+            parent = sorted(nodes.items())[prev_prnt_idx]
+            parent_depth = parent[1][1]
+            temp.append(nums[idx])
+            for t in temp:
+                nodes[t].append(parent[0])
+                nodes[t].append(parent_depth+1)
+            prev_prnt_idx += 1
             temp = []
         else:
-            temp.append(num)
-    
+            temp.append(nums[idx])
+    find_cousins(nodes, k)
+            
+for c in cousins:
+    print(c)
