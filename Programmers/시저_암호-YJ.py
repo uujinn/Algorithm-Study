c0c68c588efc538873601bf189c3e88b38988f65
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-04-12 11:15:05 +0900

    Non-pass: Programmers 시저 암호

--- Programmers/시저_암호-YJ.py
+++ Programmers/시저_암호-YJ.py
@@ -3,14 +3,21 @@ def solution(s, n):
     for alpha in s:
         if alpha == ' ':
             answer += alpha
-            continue
-        elif alpha == 'z' or alpha == 'Z':
+            # continue
+        elif 97 <= ord(alpha) < 122 or 65 <= ord(alpha) < 90:
+            answer += chr(ord(alpha) + n)
+        # elif ord(alpha) + n >= ord('z') or ord(alpha) + n >= ord('Z'):
+        #     answer += chr(ord(alpha) + n - 26)
+            # continue
+        else:
             answer += chr(ord(alpha) + n - 26)
-            continue
-        answer += chr(ord(alpha) + n)
     return answer
 
 
-print(solution("AaZz", 25))  # ZzYy
-# print(solution("a    b", 1)) #b    c
-# print(solution("a b ", 1) #b c
+# print(solution("AaZz", 25))  # ZzYy
+# print(solution("a    b", 1))  # b    c
+print(solution("a b ", 1))  # b c
+print(ord('a'))
+print(ord('z'))
+print(ord('A'))
+print(ord('Z'))
