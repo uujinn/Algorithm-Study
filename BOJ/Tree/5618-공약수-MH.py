cfc576a9a71db36e16f3608b8b1ddde6584ebaa9
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-04-20 00:01:57 +0900

    Pass: Boj 5618 공약수

---
+++ BOJ/Tree/5618-공약수-MH.py
@@ -0,0 +1,15 @@
+from pickle import FALSE
+from sys import stdin
+
+n = int(stdin.readline())
+numbers = list(map(int, stdin.readline().split()))
+
+for i in range(1, min(numbers) + 1):
+    flag = True
+    
+    for num in numbers:
+        if num % i != 0:
+            flag = False
+                
+    if flag:
+        print(i)
