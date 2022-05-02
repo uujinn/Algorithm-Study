851bcffe635b824a8c50ddd2ae14ed833d82385b
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-05-02 15:19:53 +0900

    update: BOJ 1934 최소공배수

--- BOJ/Math/1934-최소공배수-YJ.py
+++ BOJ/Math/1934-최소공배수-YJ.py
@@ -5,3 +5,11 @@ T = int(input())
 for _ in range(T):
     a, b = map(int, input().split())
     print(math.lcm(a, b))
+
+def gcd(a, b):
+    while b:
+        x, y = y, x % y
+        return x
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
