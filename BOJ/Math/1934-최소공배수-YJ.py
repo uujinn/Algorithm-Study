f6b81617197b124876924a6fd2a003f737f6a7d5
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-05-02 15:21:58 +0900

    Revert "update: BOJ 1934 최소공배수"
    This reverts commit 851bcffe635b824a8c50ddd2ae14ed833d82385b.

--- BOJ/Math/1934-최소공배수-YJ.py
+++ BOJ/Math/1934-최소공배수-YJ.py
@@ -5,11 +5,3 @@ T = int(input())
 for _ in range(T):
     a, b = map(int, input().split())
     print(math.lcm(a, b))
-
-def gcd(a, b):
-    while b:
-        x, y = y, x % y
-        return x
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
