a78e51db708a0a8d10d2f40ef4440effb3eb0213
hyeon <97775368+0ee9@users.noreply.github.com>
2022-05-06 23:20:41 +0900

    Pass: Boj 11653 소인수분해

---
+++ BOJ/Data_Structure/11653-소인수분해-GH.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+num = int(input())
+
+while num != 1:
+    for i in range(2, num + 1):
+
+        if(num % i == 0):
+            print(i)
+            num = num // i
+            break
