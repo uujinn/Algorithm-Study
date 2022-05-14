7f04b122a49d05b43f077753225508cf5f2103e8
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-05-15 01:25:40 +0900

    Pass: Boj 2960 에라토스테네스의 체

--- BOJ/Math/2960-에라토스테네스의_체-MH.py
+++ BOJ/Math/2960-에라토스테네스의_체-MH.py
@@ -4,26 +4,20 @@ N, K = map(int, stdin.readline().split())
 arr = [False, False] + [True] * (N - 1)
 count = 0
 
-for i in range(2, int(N ** 0.5) + 1):
-    if arr[i]:
+for i in range(2, N + 1):
+    if arr[i]:        
+        arr[i] = False
         count += 1
+        
         if count == K:
             print(i)
             break
         
         for j in range(i * 2, N + 1, i):
-            print('count : ', end='')
-            print(count, end=', ')
-            print(j)
-                
             if arr[j]:
+                arr[j] = False
                 count += 1
-                
-            arr[j] = False
             
             if count == K:
                 print(j)
                 break
-            
-            # print('count : ', end='')
-            # print(count)
