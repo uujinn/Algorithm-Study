ae0d55dde0d4f10db62572070f28bf65bae45689
hyeon <97775368+0ee9@users.noreply.github.com>
2022-05-25 16:06:43 +0900

    Pass: Boj 21275 폰 호석만

---
+++ BOJ/Data_Structure/21275-폰_호석만-GH.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+A, B = input().split()
+x, a, b = 0, 0, 0
+count = 0
+
+
+for i in range(2, 36 + 1):
+    try:
+        a_ = int(A, i)
+    except:
+        continue
+
+    for j in range(2, 36 + 1):
+        try:
+            b_ = int(B, j)
+            if a_ == b_:
+                count += 1
+                x = a_
+                a = i
+                b = j
+        except:
+            continue
+
+if count == 1:
+    print(x, a, b)
+
+elif count == 0:
+    print("Impossible")
+else:
+    print("Multiple")
