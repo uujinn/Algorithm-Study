33a829b4c982f9bc79178662dab863526be181aa
n-y-kim <nyn2265@gmail.com>
2022-05-30 10:04:32 +0900

    Update: 22943 수

--- BOJ/Math/22943-수-NY.py
+++ BOJ/Math/22943-수-NY.py
@@ -2,49 +2,34 @@ from sys import stdin
 from itertools import permutations
 
 def return_primes(n):
-    prime = [True] * (n+1)
+    prime = [True] * (n)
     prime[0] = prime[1] = False
     for i in range(2, int(n**0.5)+1):
         if prime[i]:
-            for j in range(i*2, n+1, i):
+            for j in range(i*2, n, i):
                 prime[j] = False
-    prime_num = []
-    for i in range(2, n+1):
-        if prime[i]: prime_num.append(i)
-    return prime_num
+    return prime
 
-def first(K, selected):
-    added = set()
-    for s in selected:
-        sum_result = sum(s)
-        s_sum = str(sum_result)
-        exist = True
-        if len(s_sum) > 1:
-            for i in range(len(s_sum)):
-                for j in range(i+1, len(s_sum)):
-                    if s_sum[i] == s_sum[j]: 
-                        exist = False
-                        break
-        if not exist: continue
-        if sum_result < 10**K and sum_result >= 10**(K-1): added.add(sum_result)
-    return added
+def first(num, prime):
+    for i in range(2, num//2+1):
+        j = num - i
+        if i != j and prime[i] and prime[j]: return True
+    return False
 
-def second(K, M, primes, selected):
-    multiply = set()
-    for p in primes:
-        if not p % M: continue
-        elif p ** 2 < 10**K: multiply.add(p**2)
-    for s in selected:
-        if s[0] % M and s[1] % M:
-            mul_result = s[0] * s[1]
-            if mul_result < 10**K and mul_result % M: multiply.add(mul_result)
-    return multiply
+def second(num, M, prime):
+    tmp = num
+    while tmp//M: tmp//=M
+    div = tmp % M
+    for i in range(2, int(div**0.5)+1):
+        if div % i == 0:
+            if prime[i] and prime[div//i]: return True
+    return False
 
 K, M = map(int, stdin.readline().split())
-primes = return_primes(10**K)
-selected = list(permutations(primes, 2))
-first = first(K, selected)
-second = second(K, M, primes, selected)
-print(first)
-print(second)
-print(len(first.intersection(second)))
+cnt = 0
+prime = return_primes(10**K)
+for num in permutations(['0', '1', '2', '3', '4', '5', '6', '7','8', '9'], K):
+    if num[0] == '0': continue
+    number = int(''.join(num))
+    if first(number, prime) and second(number, M, prime): cnt += 1
+print(cnt)
