3fdd060ebf0e181b54ee3f7ff4c636b9c47d09ff
n-y-kim <nyn2265@gmail.com>
2022-05-30 09:35:28 +0900

    Update: BOJ 22943 수

--- BOJ/Math/22943-수-NY.py
+++ BOJ/Math/22943-수-NY.py
@@ -17,7 +17,16 @@ def first(K, selected):
     added = set()
     for s in selected:
         sum_result = sum(s)
-        if sum_result < 10**K: added.add(sum_result)
+        s_sum = str(sum_result)
+        exist = True
+        if len(s_sum) > 1:
+            for i in range(len(s_sum)):
+                for j in range(i+1, len(s_sum)):
+                    if s_sum[i] == s_sum[j]: 
+                        exist = False
+                        break
+        if not exist: continue
+        if sum_result < 10**K and sum_result >= 10**(K-1): added.add(sum_result)
     return added
 
 def second(K, M, primes, selected):
@@ -28,7 +37,7 @@ def second(K, M, primes, selected):
     for s in selected:
         if s[0] % M and s[1] % M:
             mul_result = s[0] * s[1]
-            if mul_result < 10**K and not mul_result % M: multiply.add(mul_result)
+            if mul_result < 10**K and mul_result % M: multiply.add(mul_result)
     return multiply
 
 K, M = map(int, stdin.readline().split())
@@ -36,4 +45,6 @@ primes = return_primes(10**K)
 selected = list(permutations(primes, 2))
 first = first(K, selected)
 second = second(K, M, primes, selected)
+print(first)
+print(second)
 print(len(first.intersection(second)))
