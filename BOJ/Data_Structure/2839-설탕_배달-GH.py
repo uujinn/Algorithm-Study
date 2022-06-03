a8d841cd699ccc7b031225f640e37eb3fad00796
hyeon <2019112529@mme.dongguk.edu>
2022-06-04 02:13:24 +0900

    Pass: Boj 2389 설탕 배달

---
+++ BOJ/Data_Structure/2839-설탕_배달-GH.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+count = 0
+
+while True:
+    if (N % 5) == 0:
+        count = count + (N//5)
+        print(count)
+        break
+    N = N-3
+    count += 1
+
+    if N < 0:
+        print("-1")
+        break
