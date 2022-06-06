51b682c9024df21ef95909b140a9f9c91802eebf
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2022-06-06 14:41:16 +0900

    Pass: Boj 2748 피보나치 수 2

--- BOJ/Tree/9489-사촌-MH.py
+++ BOJ/Tree/9489-사촌-MH.py
@@ -6,27 +6,11 @@ while True:
     if n == 0:
         break
     
-    tree = [[] for _ in range(n)]
-    nodes = list(map(int, stdin.readline().split()))
+    nodes = [-1]        # 인덱스 맞추기 위함
+    nodes.extend(list(map(int, stdin.readline().split())))
     # nodes : [1, 3, 4, 5, 8, 9, 15, 30, 31, 32]
     print(nodes)
-    print(tree)
     
     depth = 0
-    for n in nodes:
-        if len(tree[0]) == 0:       # root에 값이 들어가면 depth 추가
-            tree[depth].append([n])
-            depth += 1
-        else:
-            print('n : ', end='')
-            print(n, end=', ')
-            
-            if tree[depth] and tree[depth][-1][-1] + 1 != n:
-                print(tree[depth][-1][-1] + 1)
-                depth += 1
-                tree[depth].append([n])
-            else:
-                tree[depth].append([n])
-            
-        
-        print(tree)
+    # 할아버지 노드가 같은 손자 노드들을 찾아서 훑기 but 부모 노드가 같으면 안됨
+    
