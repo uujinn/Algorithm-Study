977fc58e08a2805cba54c3e0697db1c611fa41c4
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-06-26 11:37:36 +0900

    Pass: Programmers 수식 최대화

--- Programmers/수식_최대화-YJ.py
+++ Programmers/수식_최대화-YJ.py
@@ -1,20 +1,33 @@
-import re
-from itertools import permutations
+
+def calc(priority, n, expression):
+
+    if n == 2:  # 가장 높은 우선순위
+        return str(eval(expression))
+    if priority[n] == '*':  # 먼저 쪼갠 애들부터 계산한다.
+        res = eval('*'.join([calc(priority, n+1, e)
+                   for e in expression.split('*')]))
+    if priority[n] == '+':
+        res = eval('+'.join([calc(priority, n+1, e)
+                   for e in expression.split('+')]))
+    if priority[n] == '-':
+        res = eval('-'.join([calc(priority, n+1, e)
+                   for e in expression.split('-')]))
+    return str(res)
 
 
 def solution(expression):
-    answer = []
-    numbers = re.split('\+|\-|\*', expression)  # 숫자
-    operations = re.split('[0-9]+', expression)[1:-1]  # 연산자
-    print(numbers)
-    print(operations)
+    answer = 0
+    priorities = [
+        ('*', '-', '+'),
+        ('*', '+', '-'),
+        ('+', '*', '-'),
+        ('+', '-', '*'),
+        ('-', '*', '+'),
+        ('-', '+', '*')
+    ]
 
-    for orders in permutations(["+", "-", "*"]):  # 우선순위
-        first_operation = orders[0]
-        second_operation = orders[1]
-        third_operation = orders[2]
+    for priority in priorities:
+        res = int(calc(priority, 0, expression))
+        answer = max(answer, abs(res))
 
     return answer
-
-
-print(solution("100-200*300-500+20"))
