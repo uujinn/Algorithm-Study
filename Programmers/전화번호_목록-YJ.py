154a8ac9b37fd0ff4962b1d7be61836d0edcf688
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-03 16:27:05 +0900

    Non-Pass: Programmers 전화번호 목록
    시간초과

--- Programmers/전화번호_목록-YJ.py
+++ Programmers/전화번호_목록-YJ.py
@@ -1,3 +1,14 @@
+from itertools import combinations
+
+
 def solution(phone_book):
-    answer = True
-    return answer
+
+    for comb in combinations(phone_book, 2):
+        p1, p2 = comb
+
+        if p1.startswith(p2) or p2.startswith(p1):
+            return False
+    return True
+
+
+print(solution(["119", "97674223", "1195524421"]))
