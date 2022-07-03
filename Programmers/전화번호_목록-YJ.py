f3c070c3c2eef3c7e098df655b327d594e6e465c
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-03 16:28:22 +0900

    Non-Pass: Programmers 전화번호 목록

--- Programmers/전화번호_목록-YJ.py
+++ Programmers/전화번호_목록-YJ.py
@@ -3,10 +3,10 @@ from itertools import combinations
 
 def solution(phone_book):
 
+    phone_book.sort()
     for comb in combinations(phone_book, 2):
         p1, p2 = comb
-
-        if p1.startswith(p2) or p2.startswith(p1):
+        if p2.startswith(p1):
             return False
     return True
 
