5e437cbd84c28d3fe8e774d8e8b1001e0040d6f1
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-12 19:09:30 +0900

    Non-Pass: Programmers 조이스틱

--- Programmers/조이스틱-YJ.py
+++ Programmers/조이스틱-YJ.py
@@ -1,11 +1,18 @@
 def solution(name):
     answer = 0
-    alpha = [min(ord(i) - ord('A'), ord('Z') - ord(i) + 1) for i in name]
+    alpha = [(ord(i) - ord('A'), ord('Z') - ord(i) + 1) for i in name]
+    arr = [0] * len(name)
 
     i = 0
 
+    currentIdx = 0
+    rightIdx = 0
+    leftIdx = 0
+
     while True:
         answer += alpha[i]
+        rightIdx = 0
+        leftIdx = 0
 
         if sum(alpha) == 0:  # 남아있는 이동거리의 합
             break
